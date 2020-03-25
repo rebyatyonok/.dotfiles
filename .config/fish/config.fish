bfdf2a4f38a7b566d106da7d936d0d632006ddc9
Ivan Petrakov <petrakvivan@gmail.com>
2020-03-25 22:25:21 +0300

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+set -g -x PATH /usr/local/bin /usr/local/sbin $HOME/.cargo/bin $PATH
+
+source ~/.config/fish/.iterm2_shell_integration.fish
+
---
+++ .config/fish/functions/doom.fish
@@ -0,0 +1,4 @@
+# Defined in /Users/ivanpetrakov/.config/fish/config.fish @ line 5
+function doom
+	~/.emacs.d/bin/doom $argv
+end
