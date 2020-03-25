4e63244ca62573fd815db2f17aa7971dce60b2f0
Ivan Petrakov <petrakvivan@gmail.com>
2020-03-25 22:34:11 +0300

    alias for managing dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ set -g -x PATH /usr/local/bin /usr/local/sbin $HOME/.cargo/bin $PATH
 
 source ~/.config/fish/.iterm2_shell_integration.fish
 
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
