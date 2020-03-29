41b1e2ecde910c5cba8a4570abc74e2c7acd6bd6
Ivan Petrakov <petrakvivan@gmail.com>
2020-03-30 00:30:24 +0300

    create alias and etc.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ set -g -x PATH /usr/local/bin /usr/local/sbin $HOME/.cargo/bin $PATH
 source ~/.config/fish/.iterm2_shell_integration.fish
 
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+alias code="code-insiders"
+
+alias ls="exa -la --group-directories-first --git"
